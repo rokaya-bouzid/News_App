94d3267980684462e7038a0f9ca20c18acc3edd0
rkia <rkiabouzid14@gmail.com>
2023-12-26 16:09:31 +0100

    Logout commit

--- app/src/main/java/kia/example/min_projet/MainActivity.kt
+++ app/src/main/java/kia/example/min_projet/MainActivity.kt
@@ -1,5 +1,6 @@
 package kia.example.min_projet
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
@@ -9,34 +10,40 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import kia.example.min_projet.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
-
-     private lateinit var binding:ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        auth = FirebaseAuth.getInstance()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment.navController
 
         // Attacher le NavController à la BottomNavigationView pour la navigation automatique
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
-
-
-
     }
 
     fun dark(item: MenuItem) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
-    fun light(item: MenuItem){
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    fun logout(item: MenuItem){
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+
+        }
+
     }
 
 }
